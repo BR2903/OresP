a62f51529da6deec559779ca6e08e60624334ed7
MAkali2 <amayabryan579@gmail.com>
2025-09-02 13:32:39 -0500

    Picar ores error

--- src/main/java/com/br2903/oresp/ModBlocks.java
+++ src/main/java/com/br2903/oresp/ModBlocks.java
@@ -10,18 +10,31 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(OresPMod.MOD_ID);
 
+    // Durezas aproximadas y resistencia a explosiones (hardness, resistance)
     public static final DeferredBlock<Block> SILVER_ORE = BLOCKS.register("silver_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE).mapColor(MapColor.COLOR_LIGHT_GRAY)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+                    .mapColor(MapColor.COLOR_LIGHT_GRAY)
+                    .strength(3.0F, 3.0F)));
     public static final DeferredBlock<Block> URANIUM_ORE = BLOCKS.register("uranium_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE).mapColor(MapColor.COLOR_GREEN)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE)
+                    .mapColor(MapColor.COLOR_GREEN)
+                    .strength(5.0F, 6.0F)));
     public static final DeferredBlock<Block> TITANIUM_ORE = BLOCKS.register("titanium_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE).mapColor(MapColor.COLOR_CYAN)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+                    .mapColor(MapColor.COLOR_CYAN)
+                    .strength(5.0F, 6.0F)));
     public static final DeferredBlock<Block> LITHIUM_ORE = BLOCKS.register("lithium_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE).mapColor(MapColor.COLOR_PINK)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+                    .mapColor(MapColor.COLOR_PINK)
+                    .strength(3.0F, 3.0F)));
     public static final DeferredBlock<Block> TIN_ORE = BLOCKS.register("tin_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE).mapColor(MapColor.COLOR_LIGHT_BLUE)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+                    .mapColor(MapColor.COLOR_LIGHT_BLUE)
+                    .strength(3.0F, 3.0F)));
     public static final DeferredBlock<Block> OIL_BLOCK = BLOCKS.register("oil_block",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.SLIME_BLOCK).mapColor(MapColor.COLOR_BLACK)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.SLIME_BLOCK)
+                    .mapColor(MapColor.COLOR_BLACK)
+                    .strength(0.5F, 0.5F)));
 
     private ModBlocks() {}
 }
--- src/main/java/com/br2903/oresp/ModItems.java
+++ src/main/java/com/br2903/oresp/ModItems.java
@@ -2,27 +2,27 @@ package com.br2903.oresp;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(OresPMod.MOD_ID);
 
-    // BlockItems para que los bloques aparezcan como ítems
-    public static final DeferredItem<Item> SILVER_ORE = ITEMS.register("silver_ore",
+    // BlockItems para que los bloques existan como ítems (y puedan dropear)
+    public static final DeferredItem<BlockItem> SILVER_ORE = ITEMS.register("silver_ore",
             () -> new BlockItem(ModBlocks.SILVER_ORE.get(), new Item.Properties()));
-    public static final DeferredItem<Item> URANIUM_ORE = ITEMS.register("uranium_ore",
+    public static final DeferredItem<BlockItem> URANIUM_ORE = ITEMS.register("uranium_ore",
             () -> new BlockItem(ModBlocks.URANIUM_ORE.get(), new Item.Properties()));
-    public static final DeferredItem<Item> TITANIUM_ORE = ITEMS.register("titanium_ore",
+    public static final DeferredItem<BlockItem> TITANIUM_ORE = ITEMS.register("titanium_ore",
             () -> new BlockItem(ModBlocks.TITANIUM_ORE.get(), new Item.Properties()));
-    public static final DeferredItem<Item> LITHIUM_ORE = ITEMS.register("lithium_ore",
+    public static final DeferredItem<BlockItem> LITHIUM_ORE = ITEMS.register("lithium_ore",
             () -> new BlockItem(ModBlocks.LITHIUM_ORE.get(), new Item.Properties()));
-    public static final DeferredItem<Item> TIN_ORE = ITEMS.register("tin_ore",
+    public static final DeferredItem<BlockItem> TIN_ORE = ITEMS.register("tin_ore",
             () -> new BlockItem(ModBlocks.TIN_ORE.get(), new Item.Properties()));
-    public static final DeferredItem<Item> OIL_BLOCK = ITEMS.register("oil_block",
+    public static final DeferredItem<BlockItem> OIL_BLOCK = ITEMS.register("oil_block",
             () -> new BlockItem(ModBlocks.OIL_BLOCK.get(), new Item.Properties()));
 
-    // Ítems simples (lingotes, etc.)
+    // Ítems de ejemplo (opcional)
     public static final DeferredItem<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> URANIUM_INGOT = ITEMS.register("uranium_ingot",
